b1b5192b0449f9c91fcdd06103e8ea743e10edf6
Amir Toloee <114802144+AmirToloee@users.noreply.github.com>
2022-11-26 16:06:26 +0330

    Update count of your word.py

--- count of your word.py
+++ count of your word.py
@@ -2,17 +2,22 @@ import numpy as np
 import pandas as pd
 import csv
 import string
+#read your file and open it
 txt = open('words1.txt').read()
+#all words convert to capital words
 txt=txt.upper()
+#split your words in list
 words = txt.split()
 data = {}
 for x in words:
         print(x)
-        data[x]=x        
+        #add your words in list to your dict 
+        data[x]=x
+        #count your words by count func and store it
         count=words.count(x)
+        #add thats count in your dic
+        #dic data remove the duplicate in your sets
         data[x]=count  
-print(data)
-mylist=list(data)
 print(data)
 s=0
 for i in range(len(txt)):
